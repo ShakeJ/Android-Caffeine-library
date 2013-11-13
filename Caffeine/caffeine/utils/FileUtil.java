618447dcc9409d98237e9a46de3a4fc207df757c
Jung Chang Hwan <shakejj@gmail.com>
2013-11-13 15:46:49 +0900

    File Utility Added

---
+++ Caffeine/caffeine/utils/FileUtil.java
@@ -0,0 +1,88 @@
+package caffeine.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+import android.net.Uri;
+import android.webkit.MimeTypeMap;
+
+public class FileUtil
+{
+   /**
+    * Get path from file
+    * @param filePath 
+    * @return
+    */
+   public static Uri uriFromFile(String filePath)
+   {
+      return Uri.fromFile(new File(filePath));
+   }
+   
+   
+   /**
+    * Get file mimeType
+    * @param filePath
+    * @return image/png, video/xxx
+    */
+   public static String mimeTypeOfFile(String filePath)
+   {
+      MimeTypeMap type = MimeTypeMap.getSingleton();
+      return type.getMimeTypeFromExtension(MimeTypeMap.getFileExtensionFromUrl(filePath));
+   }
+   
+   
+   /**
+    * Copy file 
+    * @param original file path
+    * @param copy file path
+    * @param is delete original file 
+    */
+   public static void copyTo(String fromFilePath, String toFilepath, boolean isDeleteOriginalFile)
+   {
+      File fromFile = new File(fromFilePath);
+      File toFile = new File(toFilepath);
+      
+      FileChannel src = null;
+      FileChannel dst = null;
+      
+      try
+      {
+         src = new FileInputStream(fromFile).getChannel();
+         dst = new FileOutputStream(toFile).getChannel();
+         src.transferTo(0, src.size(), dst);
+//			dst.transferFrom(src, 0, src.size());
+         src.close();
+         dst.close();
+         
+         if (isDeleteOriginalFile)
+            fromFile.delete();
+      } catch (FileNotFoundException e)
+      {
+         e.printStackTrace();
+      } catch (IOException e)
+      {
+         e.printStackTrace();
+      } finally
+      {
+         try
+         {
+            src.close();
+         } catch (IOException e)
+         {
+            e.printStackTrace();
+         }
+         
+         try
+         {
+            dst.close();
+         } catch (IOException e)
+         {
+            e.printStackTrace();
+         }
+      }
+   }
+}
--- Demo/src/com/shakej/caffeine/library/activities/MainActivity.java
+++ Demo/src/com/shakej/caffeine/library/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.shakej.caffeine.library.activities;
 
+import caffeine.utils.ContextUtil;
+
 import com.shakej.caffeine.library.R;
 import com.shakej.caffeine.library.controllers.MainListAdapter;
 
@@ -18,6 +20,8 @@ public class MainActivity extends Activity
    {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.act_main);
+      
+      ContextUtil.CONTEXT = this;
       
       _listView = (ListView) findViewById(R.id.listview_menu);
       _listAdapter = new MainListAdapter();
